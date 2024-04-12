13d1a1816faf97f2b63946c36d297deca1433c95
CitralFlo <wojtas.michal90@gmail.com>
2024-04-12 03:24:28 +0200

    Update README.md Create tests.

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # XmlDataManager
 
-In presented case....
+In presented case database is setup in "data" folder. Each file in directories: External and Internal is a separate .xml file containing data about one person.
+
+## Installation
+
+Copy the repository to your local machine.
+Use Maven to build dependencies.
+Then run `XmlDataManagerApplication.java` file.
+
+## Usage
+
+The application allows to read data from .xml files and display it in console. It also allows to add new data to the files.
+Allows to modify or delete files. Allows to search for data in files giving back path to correct file.
+
+## TODO
+
+- [ ] Create XmlService interface
+- [ ] Scan directories for files instead of hardcoding them
+- [ ] Simplyfy code inside XmlService
--- src/test/java/dev/citralflo/xmldataloader/XmlDataLoaderApplicationTests.java
+++ src/test/java/dev/citralflo/xmldataloader/XmlDataLoaderApplicationTests.java
@@ -1,7 +1,10 @@
 package dev.citralflo.xmldataloader;
 
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class XmlDataLoaderApplicationTests {
@@ -10,4 +13,42 @@ class XmlDataLoaderApplicationTests {
     void contextLoads() {
     }
 
+    @Test
+    void testPerson() {
+        Person person = new Person("Michal", "Wojtas", "wojtas.michal90@gmail.com", "19203798", "12345677701", ContractType.INTERNAL);
+
+        assertEquals("Michal", person.getFirstName());
+        assertEquals("Wojtas", person.getLastName());
+        assertEquals("wojtas.michal90@gmail.com", person.getEmail());
+        assertEquals("19203798", person.getPhoneNumber());
+        assertEquals("12345677701", person.getPesel());
+        assertEquals(ContractType.INTERNAL, person.getType());
+    }
+
+    @Test
+    void findPersonBySurname() throws ParserConfigurationException {
+        XmlService xmlService = new XmlService();
+        Person person = xmlService.findPersonBySurname("Doe");
+
+        assertEquals("John", person.getFirstName());
+        assertEquals("Doe", person.getLastName());
+        assertEquals("john.doe@gmail.com", person.getEmail());
+        assertEquals("123456789", person.getPhoneNumber());
+        assertEquals("97102655959", person.getPesel());
+    }
+
+    @Test
+    void createPersonFile() throws ParserConfigurationException, TransformerException {
+        XmlService xmlService = new XmlService();
+        xmlService.createPerson(new Person("Michal", "Wojtas", "wojtas@gmail.com","726802258" , "90010112345", ContractType.INTERNAL));
+        Person person = xmlService.findPersonBySurname("Wojtas");
+
+        assertEquals("Michal", person.getFirstName());
+        assertEquals("Wojtas", person.getLastName());
+        assertEquals("wojtas@gmail.com", person.getEmail());
+        assertEquals("726802258", person.getPhoneNumber());
+        assertEquals("90010112345", person.getPesel());
+
+
+    }
 }
